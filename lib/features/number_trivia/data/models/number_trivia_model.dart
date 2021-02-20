5ac40f92efa780c7a6da67da2040c9c7ac124b1d
Marcos Luiz <marcosluizss@gmail.com>
2021-02-20 17:12:00 -0300

    Incio da criação da implementação do repositorio - Adiciona classe de repositório - Adiciona datasources classes abstratas - Adiciona networkInfo abstract para verificação de internet - Adiciona exceptions

--- lib/features/number_trivia/data/models/number_trivia_model.dart
+++ lib/features/number_trivia/data/models/number_trivia_model.dart
@@ -1,4 +1,4 @@
-import 'package:clean_architecture_app/features/number_trivia/domain/entities/number_trivia.dart';
+import '../../domain/entities/number_trivia.dart';
 import 'package:meta/meta.dart';
 
 class NumberTriviaModel extends NumberTrivia {
---
+++ lib/features/number_trivia/data/repositories/number_trivia_repository_impl.dart
@@ -0,0 +1,32 @@
+import 'package:dartz/dartz.dart';
+import 'package:meta/meta.dart';
+
+import 'package:clean_architecture_app/core/platform/network_info.dart';
+import 'package:clean_architecture_app/features/number_trivia/data/datasources/number_trivia_local_data_source.dart';
+import 'package:clean_architecture_app/features/number_trivia/data/datasources/number_trivia_remote_data_source.dart';
+
+import '../../../../core/error/failures.dart';
+import '../../domain/entities/number_trivia.dart';
+import '../../domain/repositories/number_trivia_repository.dart';
+
+class NumberTriviaRepositoryImpl implements NumberTriviaRepository {
+  final NumberTriviaRemoteDataSource remoteDataSource;
+  final NumberTriviaLocalDataSource localDataSource;
+  final NetworkInfo networkInfo;
+
+  NumberTriviaRepositoryImpl({
+    @required this.remoteDataSource,
+    @required this.localDataSource,
+    @required this.networkInfo,
+  });
+
+  @override
+  Future<Either<Failure, NumberTrivia>> getConcreteNumberTrivia(int number) {
+    return null;
+  }
+
+  @override
+  Future<Either<Failure, NumberTrivia>> getRandomNumberTrivia() {
+    return null;
+  }
+}
