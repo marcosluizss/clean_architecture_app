9c4b7838515dbe45597e46b5c6a5f23175a7798d
Marcos Luiz <marcosluizss@gmail.com>
2021-02-20 16:37:14 -0300

    implementa a classe NumberTriviaModel - adiciona a classe - adiciona os teste de extends pra classe mae - adiciona o teste de parse - adiciona arquivos de apoio para o teste

---
+++ test/features/number_trivia/data/models/number_trivia_model_test.dart
@@ -0,0 +1,54 @@
+import 'dart:convert';
+
+import 'package:clean_architecture_app/features/number_trivia/data/models/number_trivia_model.dart';
+import 'package:clean_architecture_app/features/number_trivia/domain/entities/number_trivia.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+import '../../../../fixtures/fixiture_reader.dart';
+
+void main() {
+  final tNumberTriviaModel = NumberTriviaModel(number: 1, text: 'Test text');
+
+  test('should be a subclass of NumberTrivia entity', () async {
+    expect(tNumberTriviaModel, isA<NumberTrivia>());
+  });
+
+  group('fromJson', () {
+    test(
+      'should return a valid model when JSON number is an integer',
+      () async {
+        final Map<String, dynamic> jsonMap = json.decode(
+          fixture('trivia.json'),
+        );
+
+        final result = NumberTriviaModel.fromJson(jsonMap);
+
+        expect(result, tNumberTriviaModel);
+      },
+    );
+
+    test(
+      'should return a valid model when JSON number is regarded a double',
+      () async {
+        final Map<String, dynamic> jsonMap = json.decode(
+          fixture('trivia_double.json'),
+        );
+
+        final result = NumberTriviaModel.fromJson(jsonMap);
+
+        expect(result, tNumberTriviaModel);
+      },
+    );
+  });
+
+  group('toJSON', () {
+    test('should return a JSON map containing the proper date', () async {
+      final result = tNumberTriviaModel.toJson();
+      final expectedMap = {
+        "text": "Test text",
+        "number": 1,
+      };
+      expect(result, expectedMap);
+    });
+  });
+}
