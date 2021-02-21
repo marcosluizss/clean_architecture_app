86b584935c677f8e0410872a8d8e3c17237f0bb4
Marcos Luiz <marcosluizss@gmail.com>
2021-02-21 16:41:01 -0300

    implementação da injeção de dependência

---
+++ lib/injection_container.dart
@@ -0,0 +1,60 @@
+import 'package:clean_architecture_app/core/network/network_info.dart';
+import 'package:clean_architecture_app/core/util/input_converter.dart';
+import 'package:clean_architecture_app/features/number_trivia/data/datasources/number_trivia_local_data_source.dart';
+import 'package:clean_architecture_app/features/number_trivia/data/datasources/number_trivia_remote_data_source.dart';
+import 'package:clean_architecture_app/features/number_trivia/data/repositories/number_trivia_repository_impl.dart';
+import 'package:clean_architecture_app/features/number_trivia/domain/repositories/number_trivia_repository.dart';
+import 'package:clean_architecture_app/features/number_trivia/domain/usecases/get_concrete_number_trivia.dart';
+import 'package:clean_architecture_app/features/number_trivia/domain/usecases/get_random_number_trivia.dart';
+import 'package:data_connection_checker/data_connection_checker.dart';
+import 'package:get_it/get_it.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:http/http.dart' as http;
+
+import 'features/number_trivia/presentation/bloc/number_trivia_bloc.dart';
+
+final sl = GetIt.instance;
+
+Future<void> init() async {
+  // Features
+  sl.registerFactory(
+    () => NumberTriviaBloc(
+      concrete: sl(),
+      random: sl(),
+      inputConverter: sl(),
+    ),
+  );
+
+  sl.registerLazySingleton(() => GetConcreteNumberTrivia(sl()));
+  sl.registerLazySingleton(() => GetRandomNumberTrivia(sl()));
+
+  sl.registerLazySingleton<NumberTriviaRepository>(
+    () => NumberTriviaRepositoryImpl(
+      remoteDataSource: sl(),
+      localDataSource: sl(),
+      networkInfo: sl(),
+    ),
+  );
+
+  sl.registerLazySingleton<NumberTriviaLocalDataSource>(
+    () => NumberTriviaLocalDataSourceImpl(
+      sharedPreferences: sl(),
+    ),
+  );
+
+  sl.registerLazySingleton<NumberTriviaRemoteDataSource>(
+    () => NumberTriviaRemoteDataSourceImpl(
+      client: sl(),
+    ),
+  );
+
+  // Core
+  sl.registerLazySingleton(() => InputConverter());
+  sl.registerLazySingleton<NetworkInfo>(() => NetworkInfoImpl(sl()));
+
+  // External
+  final sharedPreferences = await SharedPreferences.getInstance();
+  sl.registerLazySingleton(() => sharedPreferences);
+  sl.registerLazySingleton(() => http.Client());
+  sl.registerLazySingleton(() => DataConnectionChecker());
+}
